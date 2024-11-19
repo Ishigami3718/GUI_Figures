0e3fe4780905e75032970d3d58dc21579873abd4
Leschenko_Rostyslav <rostishkaleschenko2006@gmail.com>
2024-11-19 11:27:50 +0200

    added classes Circle, Square, Rhomb. Circle ovverride methods implementation

---
+++ GUI_Figures/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_Figures
+{
+    internal class Circle:Figure
+    {
+        private int radius;
+        public override void DrawBlack(Graphics g)
+        {
+            g.DrawEllipse(new Pen(Color.Black), xCentre, yCentre, radius, radius);
+            g.FillEllipse(new SolidBrush(Color.Black), xCentre, yCentre, radius, radius);
+        }
+        public override void HideDrawingBackGround(Graphics g)
+        {
+            g.DrawEllipse(new Pen(Form1.DefaultBackColor), xCentre, yCentre, radius, radius);
+            g.FillEllipse(new SolidBrush(Form1.DefaultBackColor), xCentre, yCentre, radius, radius);
+        }
+    }
+}
