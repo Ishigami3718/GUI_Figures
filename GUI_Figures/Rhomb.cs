4700f72a2fc2e3a3b24c0e99c21d3b17620c3a6b
Leschenko_Rostyslav <rostishkaleschenko2006@gmail.com>
2024-11-19 11:41:48 +0200

    Rhomb ovverride methods implementtion

--- GUI_Figures/Rhomb.cs
+++ GUI_Figures/Rhomb.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace GUI_Figures
 {
-    internal class Rhomb
+    internal class Rhomb:Figure
     {
+        private int horDiagLen;
+        private int vertDiagLen;
+
+        public override void DrawBlack(Graphics g)
+        {
+            g.DrawPolygon(new Pen(Color.Black),new Point[] {new Point(xCentre-horDiagLen/2,yCentre), 
+                new Point(xCentre,yCentre-vertDiagLen/2),
+                new Point(xCentre+horDiagLen/2,yCentre),
+                new Point(xCentre,yCentre+vertDiagLen/2)});
+            g.FillPolygon(new SolidBrush(Color.Black), new Point[] {new Point(xCentre-horDiagLen/2,yCentre),
+                new Point(xCentre,yCentre-vertDiagLen/2),
+                new Point(xCentre+horDiagLen/2,yCentre),
+                new Point(xCentre,yCentre+vertDiagLen/2)});
+        }
+        public override void HideDrawingBackGround(Graphics g)
+        {
+            g.DrawPolygon(new Pen(Form1.DefaultBackColor), new Point[] {new Point(xCentre-horDiagLen/2,yCentre),
+                new Point(xCentre,yCentre-vertDiagLen/2),
+                new Point(xCentre+horDiagLen/2,yCentre),
+                new Point(xCentre,yCentre+vertDiagLen/2)});
+            g.FillPolygon(new SolidBrush(Form1.DefaultBackColor), new Point[] {new Point(xCentre-horDiagLen/2,yCentre),
+                new Point(xCentre,yCentre-vertDiagLen/2),
+                new Point(xCentre+horDiagLen/2,yCentre),
+                new Point(xCentre,yCentre+vertDiagLen/2)});
+        }
     }
 }
