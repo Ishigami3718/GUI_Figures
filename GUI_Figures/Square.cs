6e57fba6f7be78c92e2b83c847c7b7f4f74a24a7
Leschenko_Rostyslav <rostishkaleschenko2006@gmail.com>
2024-11-19 11:32:39 +0200

    Square ovverride methods implementation

--- GUI_Figures/Square.cs
+++ GUI_Figures/Square.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace GUI_Figures
 {
-    internal class Square
+    internal class Square:Figure
     {
+        private int sideLength;
+        public override void DrawBlack(Graphics g)
+        {
+            g.DrawRectangle(new Pen(Color.Black),xCentre,yCentre,sideLength,sideLength);
+            g.FillRectangle(new SolidBrush(Color.Black), xCentre, yCentre, sideLength, sideLength);
+        }
+        public override void HideDrawingBackGround(Graphics g)
+        {
+            g.DrawRectangle(new Pen(Form1.DefaultBackColor), xCentre, yCentre, sideLength, sideLength);
+            g.FillRectangle(new SolidBrush(Form1.DefaultBackColor), xCentre, yCentre, sideLength, sideLength);
+        }
     }
 }
